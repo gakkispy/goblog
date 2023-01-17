696beed45174accaabde923747dbcd9934101b5f
gakkispy <yaosenjun168@live.cn>
2023-01-17 09:32:53 +0800

    404 状态码

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * @Date: 2023-01-16 14:28:24
  * @LastEditors: gakkispy && yaosenjun@cii.com
- * @LastEditTime: 2023-01-17 09:24:39
+ * @LastEditTime: 2023-01-17 09:30:24
  * @FilePath: /goblog/main.go
  */
 package main
@@ -19,6 +19,7 @@ func handleFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "此博客由 Go 语言编写，使用 Gin 框架，使用 GORM 操作数据库，使用 PostgreSQL 数据库。联系方式："+
 			"<a href='mailto:yaosenjun168@live.cn'>yaosenjun168@live.cn</a>。")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>Hello, 这里是 goblog 404 page !</h1>")
 	}
 }
