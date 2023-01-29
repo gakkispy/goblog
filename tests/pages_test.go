760d4ed6cc2c6fbe26aa2b4e9e84b9c3ee304b38
gakkispy <yaosenjun168@live.cn>
2023-01-29 10:50:51 +0800

    完成代码测试

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -1,29 +1,60 @@
 /*
  * @Date: 2023-01-29 09:37:45
  * @LastEditors: gakkispy && yaosenjun168@live.cn
- * @LastEditTime: 2023-01-29 09:41:36
+ * @LastEditTime: 2023-01-29 10:50:16
  * @FilePath: /goblog/tests/pages_test.go
  */
 package tests
 
 import (
 	"net/http"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHomePage(t *testing.T) {
+func TestAllPages(t *testing.T) {
 	baseURL := "http://localhost:3000"
 
-	// 1. 请求 -- 模拟用户访问浏览器
-	var (
-		resp *http.Response
-		err  error
-	)
-	resp, err = http.Get(baseURL + "/")
+	// 1. 声明加初始化测试数据
+	var tests = []struct {
+		url      string
+		method   string
+		expected int
+	}{
+		{"/", "GET", http.StatusOK},
+		{"/about", "GET", http.StatusOK},
+		{"/notfound", "GET", http.StatusNotFound},
+		{"/articles", "GET", http.StatusOK},
+		{"/articles/2", "GET", http.StatusOK},
+		{"/articles/2/edit", "GET", http.StatusOK},
+		{"/articles/create", "GET", http.StatusOK},
+		{"/articles/2", "POST", http.StatusOK},
+		{"/articles/", "POST", http.StatusOK},
+		{"/articles/1/delete", "POST", http.StatusNotFound},
+	}
 
-	// 2. 断言 -- 检查响应是否符合预期
-	assert.NoError(t, err, "有错误发生，错误信息为：%v", err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode, "响应状态码不是 200")
+	// 2. 遍历测试数据
+	for _, test := range tests {
+		t.Logf("当前测试的URL: %v \n", test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
+
+		// 2.1 发起请求
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+
+		// 2.2 断言
+		assert.NoError(t, err, "请求 "+test.url+" 时有错误")
+		assert.Equal(t, test.expected, resp.StatusCode, "请求 "+test.url+" 时状态码错误，应为 "+strconv.Itoa(test.expected)+"")
+	}
 }
